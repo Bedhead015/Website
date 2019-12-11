fb6cb5811cc212a7dbfefd96300410babf7dda70
Josh Anderson <bedhead015@pop-os.localdomain>
2019-12-11 06:13:10 -0700

    Added Pics and Src directories

---
+++ Src/js/script.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+} 
+
+function openNav() {
+   // document.getElementById("mySidenav").style.width = "250px";
+   alert("It Worked!");
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+} 
